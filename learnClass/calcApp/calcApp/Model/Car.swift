8c02483b6390880f450ed5f7d58a142e01c51748
長坂豪士 <nagasakatsuyoshi@nagasakagoushinoMacBook-Air.local>
2019-09-30 20:44:02 +0900

    second commit

--- learnClass/calcApp/calcApp/Model/Car.swift
+++ learnClass/calcApp/calcApp/Model/Car.swift
@@ -15,13 +15,24 @@ class Car {
     var rearWheel = 0
     
     init() {
-        frontWheel = 2
-        rearWheel = 2
+        frontWheel = 0
+        rearWheel = 0
         
     }
     
     func drive() {
         
+        print("運転開始！！")
+        print("前輪　　\(frontWheel)")
+        print("前輪　　\(rearWheel)")
+    }
+    
+    func move(toBack: String) {
+        print(toBack)
+    }
+    
+    func plusAndMinus (num1:Int, num2:Int) -> Int {
+        return num1 + num2
     }
     
 }
--- learnClass/calcApp/calcApp/ViewController.swift
+++ learnClass/calcApp/calcApp/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Car()クラスを継承したcarModelインスタンスを生成
+    var carModel = Car()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        carModel.frontWheel = 10
+        carModel.rearWheel = 10
+        
+        
     }
 
+    
+    @IBAction func doAction(_ sender: Any) {
+        
+        // Carクラスのdrive()メソッドを呼び出し
+        carModel.drive()
+        carModel.move(toBack: "後ろに行くよ！")
+        let total = carModel.plusAndMinus(num1: carModel.frontWheel, num2: carModel.rearWheel)
+        print("タイヤの合計数   \(total)")
+    }
+    
+    
 
 }
 
