1ef2a3d1b3cb582af3dbc2bef3a7b9d12ad6cf06
長坂豪士 <nagasakatsuyoshi@nagasakagoushinoMacBook-Air.local>
2019-10-04 14:59:14 +0900

    first commit

---
+++ seguePractice/seguePractice/nextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  nextViewController.swift
+//  seguePractice
+//
+//  Created by 長坂豪士 on 2019/10/01.
+//  Copyright © 2019 Tsuyoshi Nagasaka. All rights reserved.
+//
+
+import UIKit
+
+class nextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
